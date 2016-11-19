cf3c680741ea3234068b9ab2fc7ee3c344e443bf
Sologin <sologin@bories.eu>
2016-11-19 10:12:10 +0100

    Add files via upload

---
+++ proj.android/obj/local/armeabi/objs-debug/cocos_extension_static/physics-nodes/CCPhysicsSprite.o.d
@@ -0,0 +1,290 @@
+obj/local/armeabi/objs-debug/cocos_extension_static/physics-nodes/CCPhysicsSprite.o: \
+ C:/Dev/medfan/proj.android/../cocos2d/extensions/physics-nodes/CCPhysicsSprite.cpp \
+ C:/Dev/medfan/proj.android/../cocos2d/extensions/physics-nodes/CCPhysicsSprite.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCSprite.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCNode.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccMacros.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCConsole.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/thread \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/mutex \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCRef.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformMacros.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccConfig.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformConfig.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformDefine.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCPlatformDefine-android.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCStdC.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCStdC-android.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cassert \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccRandom.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/random \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCVector.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCProtocols.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccTypes.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCGeometry.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMath.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMathBase.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGL.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCGL-android.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTexture2D.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCScriptSupport.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCCommon.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCTouch.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventTouch.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEvent.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventKeyboard.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/list \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCAffineTransform.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponentContainer.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCMap.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponent.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsBody.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsShape.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCDrawNode.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCCustomCommand.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCRenderCommand.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTextureAtlas.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTrianglesCommand.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCGLProgramState.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCAutoPolygon.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCImage.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../extensions/ExtensionMacros.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../extensions/ExtensionExport.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventListenerCustom.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventListener.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCDirector.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/stack \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCScene.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGLView.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventDispatcher.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/set \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/chipmunk.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/chipmunk_types.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpVect.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpBB.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpTransform.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSpatialIndex.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpArbiter.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpBody.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpShape.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpPolyShape.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpConstraint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpPinJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSlideJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpPivotJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpGrooveJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpDampedSpring.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpDampedRotarySpring.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpRotaryLimitJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpRatchetJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpGearJoint.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSimpleMotor.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSpace.h \
+ C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpHastySpace.h
+
+C:/Dev/medfan/proj.android/../cocos2d/extensions/physics-nodes/CCPhysicsSprite.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCSprite.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCNode.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccMacros.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCConsole.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/thread:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/mutex:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCRef.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformMacros.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccConfig.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformConfig.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformDefine.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCPlatformDefine-android.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCStdC.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCStdC-android.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cassert:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccRandom.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/random:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCVector.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCProtocols.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccTypes.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCGeometry.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMath.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMathBase.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGL.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCGL-android.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTexture2D.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCScriptSupport.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCCommon.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCTouch.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventTouch.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEvent.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventKeyboard.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/list:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCAffineTransform.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponentContainer.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCMap.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponent.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsBody.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsShape.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCDrawNode.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCCustomCommand.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCRenderCommand.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTextureAtlas.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTrianglesCommand.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCGLProgramState.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCAutoPolygon.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCImage.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../extensions/ExtensionMacros.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../extensions/ExtensionExport.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventListenerCustom.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventListener.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCDirector.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/stack:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCScene.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGLView.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventDispatcher.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/set:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/chipmunk.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/chipmunk_types.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpVect.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpBB.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpTransform.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSpatialIndex.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpArbiter.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpBody.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpShape.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpPolyShape.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpConstraint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpPinJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSlideJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpPivotJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpGrooveJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpDampedSpring.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpDampedRotarySpring.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpRotaryLimitJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpRatchetJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpGearJoint.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSimpleMotor.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpSpace.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/external/chipmunk/prebuilt/android/../../include/chipmunk/cpHastySpace.h:
