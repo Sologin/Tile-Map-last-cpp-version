2f11ebab56f5f1b5be3560017ce588b012956f36
Sologin <sologin@bories.eu>
2016-11-19 10:28:52 +0100

    Add files via upload

---
+++ proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/2d/CCAutoPolygon.o.d
@@ -0,0 +1,233 @@
+obj/local/armeabi/objs-debug/cocos2dx_internal_static/2d/CCAutoPolygon.o: \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCAutoPolygon.cpp \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCAutoPolygon.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCImage.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCRef.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformMacros.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccConfig.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformConfig.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformDefine.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCPlatformDefine-android.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTexture2D.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCGeometry.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccMacros.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCConsole.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/thread \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/mutex \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCStdC.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCStdC-android.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cassert \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccRandom.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/random \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMath.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMathBase.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.inl \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccTypes.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGL.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCGL-android.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTrianglesCommand.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCRenderCommand.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCGLProgramState.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCVector.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/poly2tri.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/common/shapes.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/cdt.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/advancing_front.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/../common/shapes.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/sweep_context.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/list \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/sweep.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCDirector.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/stack \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCScene.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCNode.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCProtocols.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCScriptSupport.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCCommon.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCTouch.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventTouch.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEvent.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventKeyboard.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCAffineTransform.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponentContainer.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCMap.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponent.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsBody.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsShape.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGLView.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTextureCache.h \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/condition_variable \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/queue \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/clipper/clipper.hpp \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/set \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/ostream
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCAutoPolygon.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCImage.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCRef.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformMacros.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccConfig.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformConfig.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformDefine.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCPlatformDefine-android.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTexture2D.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/unordered_map:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCGeometry.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccMacros.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCConsole.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/thread:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/mutex:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCStdC.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCStdC-android.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cassert:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccRandom.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/random:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMath.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCMathBase.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec2.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec3.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Vec4.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Mat4.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/Quaternion.inl:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/ccTypes.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGL.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCGL-android.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTrianglesCommand.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCRenderCommand.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCGLProgramState.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCVector.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/poly2tri.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/common/shapes.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/cdt.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/advancing_front.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/../common/shapes.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/sweep_context.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/list:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/poly2tri/sweep/sweep.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCDirector.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/stack:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCScene.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCNode.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCProtocols.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCScriptSupport.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCCommon.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCTouch.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventTouch.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEvent.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCEventKeyboard.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./math/CCAffineTransform.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponentContainer.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./base/CCMap.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCComponent.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsBody.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./physics/CCPhysicsShape.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGLView.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./renderer/CCTextureCache.h:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/condition_variable:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/queue:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./../external/clipper/clipper.hpp:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/set:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+C:/Logiciels/android-ndk-r11c/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/ostream:
---
+++ proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/2d/CCGLBufferedNode.o.d
@@ -0,0 +1,14 @@
+obj/local/armeabi/objs-debug/cocos2dx_internal_static/2d/CCGLBufferedNode.o: \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCGLBufferedNode.cpp \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCGLBufferedNode.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGL.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformConfig.h \
+ C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCGL-android.h
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./2d/CCGLBufferedNode.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCGL.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/CCPlatformConfig.h:
+
+C:/Dev/medfan/proj.android/../cocos2d/cocos/./platform/android/CCGL-android.h:
